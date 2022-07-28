ee0c6fd1a1fd1930ed2e50c9c08a3138f8d25898
Amani Kiruga <amaniedu71@gmail.com>
2022-07-27 21:00:50 -0400

    add react+tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    //create large text box to input text tailwind
+    return (
+        <div className="App">
+            <input type="text" className="text-xl font-bold" />
+        </div>
+    );
+}
+
+export default App;
