9861326d8a0d1f5e96532d18d4da30e70d24ccec
Amani Kiruga <amaniedu71@gmail.com>
2022-07-27 21:07:57 -0400

    tailwind stop breaking

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ function App() {
     //create large text box to input text tailwind
     return (
         <div className="App">
-            <input type="text" className="text-xl font-bold" />
+            <h1 className="text-3xl font-bold">CPC OJ</h1>
+            <input
+                type="text"
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            />
         </div>
     );
 }
